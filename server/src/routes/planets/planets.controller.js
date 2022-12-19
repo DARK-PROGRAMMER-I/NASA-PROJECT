e60dd9986acf46ca3410886d2fa38c4e62f3adc4
NewUser <hammadmalli64@gmail.com>
2022-12-19 22:49:48 +0500

    get planets

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -3,3 +3,7 @@ const planets = require("../../models/planets.models.js");
 function getAllPlanets(req, resp){
       return resp.status(200).json(planets);
 }
+
+module.exports = {
+      getAllPlanets
+}
--- server/src/routes/planets/planets.routes.js
+++ server/src/routes/planets/planets.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const [getAllPlanets] = require("")
 
 const planetRouter = express.Router();
 
