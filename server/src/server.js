9ed675c2ce1c6c2cf92b9ee10ec4f754fbf0836e
NewUser <hammadmalli64@gmail.com>
2022-12-19 21:55:19 +0500

    started

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,12 @@
 // In this file we will keep all the server-related info
 const http = require("http");
 
+const PORT = 8000;
 
-http.c
+const server = http.createServer();
+
+server.listen(PORT, ()=>{
+      console.log("Listening to port " + PORT.toString);
+})
+
+
