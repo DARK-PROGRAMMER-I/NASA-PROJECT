69ee71dd5ceda35eaec55753a74dda7f21266976
NewUser <hammadmalli64@gmail.com>
2022-12-19 22:29:06 +0500

    routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const planetRouter = require("./routes/planets/planets.routes.js");
 
 const app = express();  
 // Adding MiddleWare
 app.use(express.json);
+
+// Planet Routes
+app.use(planetRouter);
 
 module.exports = app;
