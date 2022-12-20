7cf34524da5912e7e5f4b1870f655610aaa524c1
NewUser <hammadmalli64@gmail.com>
2022-12-20 14:08:57 +0500

    facing issue

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const planetRouter = require("./routes/planets/planets.routes");
 
 const app = express();  
 
 // We will add Cors Middleware here
+app.use(cors({
+      origin: "http://localhost:8000"
+}))
+
 // Adding MiddleWare
 app.use(express.json);
 
--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,7 @@
 const http = require("http");
 const app = require("./app.js");
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000; //process.env.PORT || 
 
 const server = http.createServer(app);
 
